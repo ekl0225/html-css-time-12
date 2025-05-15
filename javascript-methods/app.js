5e3a673ffa19fdc1f3d136fd44c412f16212bcd4
emilia <ekl022025@gmail.com>
2025-05-15 08:58:46 +0200

    jdks

---
+++ javascript-methods/app.js
@@ -0,0 +1,47 @@
+// methods - innebygde verktøy i javascript
+
+//string methods
+
+const text = "Dette er text.   ";
+const text2 = " Mer text.";
+
+// const textInUppercase = text.toUpperCase();
+
+// console.log(text.toLowerCase());
+// console.log(text.toUpperCase());
+// Property
+// console.log(text.length);
+// console.log(text.slice(-7, -2));
+// console.log(text.substring(6));
+// console.log(text.replace("Dette", "Her"));
+// console.log(text.concat(text2));
+// console.log(text + text2);
+//console.log(text.trim());
+// console.log(text.charAt(1));
+//console.log(text[0]); //henter ut konkret index i string
+//console.log(text.charCodeAt(0));
+//console.log(text.split(" ")); //klipper ut
+
+// Numbers
+
+const number = 247.1926;
+
+// console.log(number.toFixed(0));
+// console.log(number.toPrecision(5));
+
+// Arrays
+
+const fruits = ["apples", "pears", "bananas", "grapes", "cherries"];
+
+//console.log(fruits.length);
+//console.log(fruits.join("-"));
+// fruits.pop(); // removes
+// console.log(fruits);
+// fruits.push("mango");  // Adds
+// console.log(fruits);
+//fruits.shift();
+// fruits.unshift("lemons");
+// console.log(fruits);
+// console.log(fruits.concat(["lemons", "limes"]));
+//fruits.splice(2, 0, "lemons", "limes");
+//console.log(fruits);
